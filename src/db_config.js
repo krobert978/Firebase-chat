3d8a235d2f02c62623085fe45745bf5616763c38
Robert Koteles <robert.koteles@freemail.hu>
2021-06-13 22:26:17 +0200

    orai allapot

---
+++ src/db_config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: 'AIzaSyC00fSeQzNxOru34w4N9P-nXPgaG9bKCJc',
+  authDomain: 'chat-app-wsuf-kr.firebaseapp.com',
+  databaseURL: 'https://chat-app-wsuf-kr.firebaseio.com',
+  projectId: 'chat-app-wsuf-kr',
+  storageBucket: 'chat-app-wsuf-kr.appspot.com',
+  messagingSenderId: '195370094444'
+};
+
+export default config;
